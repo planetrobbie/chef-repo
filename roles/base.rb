b5708fba196c3183e9f52241c08dcbb3bbc62c68
Sébastien Braun <sebbraun@gmail.com>
2012-12-23 22:43:23 +0100

    sudo recipe added

--- roles/base.rb
+++ roles/base.rb
@@ -4,12 +4,13 @@ description "Role with configuration common to all nodes"
 run_list(
   "recipe[apt]",
   "recipe[chef-client]",
-  "recipe[bootstrap]"
+  "recipe[bootstrap]",
+  "recipe[sudo]"
 )
 override_attributes(
   :authorization => {
 	:sudo => {
-	  :users => ["brauns"],
+	  :users => ["ubuntu", "brauns"],
 	  :passwordless => true
 	}
   }
